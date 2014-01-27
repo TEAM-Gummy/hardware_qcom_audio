834ab283569958a885ba3e3cc1e37407c1de5bc2
cphelps76 <cphelps76@gmail.com>
2014-01-27 15:26:17 -0600

    more hardware unbreaking, temp patch
    Change-Id: Ie43589a965d73d9a84593caedeec23504eeb3d2d

--- hal/msm8960/platform.c
+++ hal/msm8960/platform.c
@@ -594,10 +594,10 @@ snd_device_t platform_get_output_snd_device(void *platform, audio_devices_t devi
         goto exit;
     }
 
-    if(devices & AUDIO_DEVICE_OUT_PROXY) {
-        ALOGD("%s: setting sink capability for Proxy", __func__);
-        audio_extn_set_afe_proxy_channel_mixer(adev);
-    }
+//    if(devices & AUDIO_DEVICE_OUT_PROXY) {
+//        ALOGD("%s: setting sink capability for Proxy", __func__);
+//        audio_extn_set_afe_proxy_channel_mixer(adev);
+//    }
 
     if (mode == AUDIO_MODE_IN_CALL) {
         if (devices & AUDIO_DEVICE_OUT_WIRED_HEADPHONE ||
@@ -672,10 +672,10 @@ snd_device_t platform_get_output_snd_device(void *platform, audio_devices_t devi
         snd_device = SND_DEVICE_OUT_USB_HEADSET;
     } else if (devices & AUDIO_DEVICE_OUT_EARPIECE) {
         snd_device = SND_DEVICE_OUT_HANDSET;
-    } else if (devices & AUDIO_DEVICE_OUT_PROXY) {
-        ALOGD("%s: setting sink capability for Proxy", __func__);
-        audio_extn_set_afe_proxy_channel_mixer(adev);
-                snd_device = SND_DEVICE_OUT_AFE_PROXY;
+//    } else if (devices & AUDIO_DEVICE_OUT_PROXY) {
+//        ALOGD("%s: setting sink capability for Proxy", __func__);
+//        audio_extn_set_afe_proxy_channel_mixer(adev);
+//                snd_device = SND_DEVICE_OUT_AFE_PROXY;
     } else {
         ALOGE("%s: Unknown device(s) %#x", __func__, devices);
     }
